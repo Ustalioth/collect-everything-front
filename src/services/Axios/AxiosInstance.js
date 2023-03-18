bc2f1797eb4173d566d647e459022d065fcc6bf6
Tai Mui CHENH <chenh.tm@gmail.com>
2023-03-18 20:00:22 +0100

    [JWT] Decode function

--- src/services/Axios/AxiosInstance.js
+++ src/services/Axios/AxiosInstance.js
@@ -2,11 +2,21 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL : process.env.REACT_APP_API_ADDRESS,
-    headers: {
-    //  Authorization: `<Your Auth Token>`,
-    //    Content-Type: "application/json",
-    //    timeout : 1000,
-    },
+    timeout: 5000,
 });
+
+instance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+  
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default instance;
